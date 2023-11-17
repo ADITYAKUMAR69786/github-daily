e27245d83eaad3f29692842370c51464ffdea498
ADITYA KUMAR VERMA <77196597+ADITYAKUMAR69786@users.noreply.github.com>
2023-11-17 18:56:25 +0530

    Updated Solution - GfG to GitHub

--- Basic/Check if strings are rotations of each other or not/check-if-strings-are-rotations-of-each-other-or-not.cpp
+++ Basic/Check if strings are rotations of each other or not/check-if-strings-are-rotations-of-each-other-or-not.cpp
@@ -11,15 +11,9 @@ class Solution
     //Function to check if two strings are rotations of each other or not.
     bool areRotations(string s1,string s2)
     {
-        // Your code here
-        if(s1.length()!=s2.length())
-        return false;
+        return(s1+s1).find(s2)!=string::npos;
         
-        s1+=s1;
-        if(s1.find(s2)!=-1)
-        return true;
-        else
-        return false;
+        
     }
 };
 
