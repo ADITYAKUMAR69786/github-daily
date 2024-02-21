2f72351dafb8dcbe8a816f201d96e4106d53007d
ADITYA KUMAR VERMA <77196597+ADITYAKUMAR69786@users.noreply.github.com>
2024-02-21 08:55:00 +0530

    Updated Solution - GfG to GitHub

--- Hard/Boolean Parenthesization/boolean-parenthesization.cpp
+++ Hard/Boolean Parenthesization/boolean-parenthesization.cpp
@@ -6,67 +6,81 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-
 class Solution{
 public:
-unordered_map<string,int> mp;
-int solve(string s,int i,int j,bool istrue)
-{
-    if(i>j)
-    return false;
-    if(i==j)
-    {
-        if(istrue==true)
-            return s[i]=='T';
-        else
-        return s[i]=='F';
+    int solve(int n,string &s,int i,int j,bool need,vector<vector<vector<int>>>&dp){
+        // the parenthesis at i means just in the lhs of the the ith index
+        if(i+1==j){
+            if(need==false){
+                return (s[i]=='F')?1:0;
+            }
+            else{
+                return (s[i]=='F')?0:1;
+            }
+        }
+        if(i>j){
+            return 0;
+        }
         
+        if(dp[i][j][need]!=-1){
+            return dp[i][j][need];
+        }
+        int ans = 0;
+        for(int k = i+1;k<j;k+=2){
+            if(s[k]=='^'){
+                if(need==true){
+                    int ct1 = solve(n,s,i,k,true,dp);//1
+                    int ct2 = solve(n,s,k+1,j,false,dp);//1
+                    int ct3 = solve(n,s,i,k,false,dp);//0
+                    int ct4 = solve(n,s,k+1,j,true,dp);//0
+                    ans = ans + ct1*ct2 + ct3*ct4;
+                }
+                else{
+                    int ct1 = solve(n,s,i,k,true,dp);
+                    int ct2 = solve(n,s,k+1,j,true,dp);
+                    int ct3 = solve(n,s,i,k,false,dp);
+                    int ct4 = solve(n,s,k+1,j,false,dp);
+                    ans = ans + ct1*ct2 + ct3*ct4;
+                }
+            }
+            else if(s[k]=='|'){
+                if(need==true){
+                    int ct1 = solve(n,s,i,k,true,dp);
+                    int ct2 = solve(n,s,k+1,j,false,dp);
+                    int ct3 = solve(n,s,k+1,j,true,dp);
+                    
+                    int ct4 = solve(n,s,i,k,false,dp);
+                    int ct5 = solve(n,s,i,k,true,dp);
+                    int ct6 = solve(n,s,k+1,j,true,dp);
+                    ans = ans + ct1*ct2+ct1*ct3+ct6*ct4;
+                }
+                else{
+                    int ct1 = solve(n,s,i,k,false,dp);//1
+                    int ct2 = solve(n,s,k+1,j,false,dp);//1
+                    ans = ans + ct1*ct2;
+                }
+            }
+            else{
+                if(need == true){
+                    int ct1 = solve(n,s,i,k,true,dp);
+                    int ct2 = solve(n,s,k+1,j,true,dp);
+                    ans = ans + ct1*ct2;
+                }
+                else{
+                    int ct1 = solve(n,s,i,k,true,dp);
+                    int ct2 = solve(n,s,k+1,j,false,dp);
+                    
+                    int ct4 = solve(n,s,i,k,false,dp);
+                    int ct5 = solve(n,s,k+1,j,true,dp);
+                    ans = ans + ct1*ct2 + ct4*ct5 + ct4*ct2;
+                }
+            }
+        }
+        return dp[i][j][need] = (ans%1003);
     }
-    string temp=to_string(i);
-    temp.push_back(' ');
-    temp.append(to_string(j));
-    temp.push_back(' ');
-    temp.append(to_string(istrue));
-    if(mp.find(temp)!=mp.end())
-    return mp[temp];
-    int ans=0;
-    for(int k=i+1;k<j;k=k+2)
-    {
-        int lt=solve(s,i,k-1,true);
-        int lf=solve(s,i,k-1,false);
-        int rt=solve(s,k+1,j,true);
-        int rf=solve(s,k+1,j,false);
-      if(s[k]=='&')
-      {
-          if(istrue==true)
-          ans=ans+lt*rt;
-          else
-          ans=ans+(lt*rf+lf*rt+lf*rf);
-      }
-      if(s[k]=='|')
-      {
-          if(istrue==true)
-          ans+=(lt*rf+lf*rt+lt*rt);
-          else
-          ans+=lf*rf;
-      }
-      if(s[k]=='^')
-      {
-          if(istrue==true)
-          ans+=lt*rf+lf*rt;
-          else
-          ans+=lt*rt+lf*rf;
-      }
-        
-    }
-    mp[temp]=ans%1003;
-    return (mp[temp])%1003;
-    
-}
-    int countWays(int n,string s){
-        bool istrue=true;
-    int x= solve(s,0,n-1,istrue);
-    return (x%1003);
+    int countWays(int n, string s){
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
+        return solve(n,s,0,s.size(),true,dp);
     }
 };
 
@@ -77,13 +91,13 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int N;
-        cin>>N;
-        string S;
-        cin>>S;
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
         
         Solution ob;
-        cout<<ob.countWays(N, S)<<"\n";
+        cout<<ob.countWays(n, s)<<"\n";
     }
     return 0;
 }
